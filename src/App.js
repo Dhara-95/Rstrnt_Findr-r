ae1f263445c113c0a8fc49417f5e683de213bedc
Dhara Patel <dhara.patel95@hotmail.co.uk>
2021-01-28 11:57:07 +0000

    Added padding to each element

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import "./App.css";
 function App() {
   return (
     <div className="App bg-black">
-      <h1 className="text-5xl text-grey" >Rstrnt Findr-r</h1>
-      <span className="text-grey">Find restaurants near you:</span>
-      <form>
-        <input name="address" type="text" />
-        <input className="submit-button" value="Find" type="submit" />
-      </form>
-      <h2 className="text-2xl text-grey">Find By Cuisine</h2>
-      <div className="image-container text-grey">
-        <div>image 1</div>
-        <div>image 2</div>
-        <div>image 3</div>
-        <div>image 4</div>
+      <h1 className="text-5xl text-grey py-6" >Rstrnt Findr-r</h1>
+      <div className="py-8">
+        <span className="text-grey">Find restaurants near you:</span>
+        <form>
+          <input name="address" type="text" />
+          <input className="submit-button" value="Find" type="submit" />
+        </form>
+      </div >
+      <div className="py-8">
+        <h2 className="text-2xl text-grey">Find By Cuisine</h2>
+        <div className="image-container text-grey">
+          <div>image 1</div>
+          <div>image 2</div>
+          <div>image 3</div>
+          <div>image 4</div>
+        </div>
       </div>
     </div>
   );
