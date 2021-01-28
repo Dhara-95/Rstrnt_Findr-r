128e1afcb2fe621417b65bc4535f5adcc8b7520a
Chris Cooney <chris.cooney05@hotmail.co.uk>
2021-01-28 14:37:36 +0000

    Styled the input field and button

--- src/App.js
+++ src/App.js
@@ -8,9 +8,18 @@ function App() {
 
       <div className="py-8 flex justify-around items-center w-1/2 mx-auto">
         <span className="text-grey">Find restaurants near you:</span>
-        <div className="rounded bg-white w-1/2 flex py-2 justify-center">
-          <input name="address" type="text" />
-          <button className="submit-button rounded" type="button" value="Find">
+        <div className="rounded bg-white w-1/2 flex py-2 px-1 justify-between">
+          <input
+            className="w-3/5 focus:outline-none"
+            name="address"
+            type="text"
+            placeholder="W13 3RH"
+          />
+          <button
+            className="submit-button rounded py-1.5 px-4"
+            type="button"
+            value="Find"
+          >
             Find
           </button>
         </div>
