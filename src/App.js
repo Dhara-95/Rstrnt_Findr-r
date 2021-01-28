d4742086ff026e699d593f8b58f9782cf5be359b
Chris Cooney <chris.cooney05@hotmail.co.uk>
2021-01-28 14:27:38 +0000

    Added styling to the input field and button

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import "./App.css";
 function App() {
   return (
     <div className="App container w-4/5">
-      <h1 className="text-5xl text-grey py-6" >Rstrnt Findr-r</h1>
-      <div className="py-8 flex justify-center">
+      <h1 className="text-5xl text-grey py-6">Rstrnt Findr-r</h1>
+
+      <div className="py-8 flex justify-around items-center w-1/2 mx-auto">
         <span className="text-grey">Find restaurants near you:</span>
-        <form>
+        <div className="rounded bg-white w-1/2 flex py-2 justify-center">
           <input name="address" type="text" />
-          <input className="submit-button" value="Find" type="submit" />
-        </form>
-      </div >
+          <button className="submit-button rounded" type="button" value="Find">
+            Find
+          </button>
+        </div>
+      </div>
+
       <div className="py-8">
         <h2 className="text-2xl text-grey">Find By Cuisine</h2>
         <div className="image-container text-grey flex justify-around">
