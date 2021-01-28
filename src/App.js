bf27c6c2bca89982f8638b9fede37ec5201d3279
Chris Cooney <chris.cooney05@hotmail.co.uk>
2021-01-28 10:00:03 +0000

    Added input field for the user to supply address

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Rstrnt Findr-r</h1>
+      <input name="address" type="text" />
     </div>
   );
 }
--- test/specs/homepage.js
+++ test/specs/homepage.js
@@ -9,4 +9,10 @@ describe("homepage", () => {
     const header = $("h1=Rstrnt Findr-r");
     expect(header).toBeDisplayed();
   });
+
+  it("Has an input field for user address/postcode", () => {
+    browser.url("http://localhost:3000");
+    const inputField = $('[name="address"]');
+    expect(inputField).toExist();
+  });
 });
