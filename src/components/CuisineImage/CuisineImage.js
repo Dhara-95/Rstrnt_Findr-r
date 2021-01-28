5683ec7c8f0f3b358d4e0533038a75279b7707df
Dhara Patel <dhara.patel95@hotmail.co.uk>
2021-01-28 15:59:54 +0000

    Split divs into components

---
+++ src/components/CuisineImage/CuisineImage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CuisineImage = (props) => {
+  const {image} = props;
+
+  return (
+  <div className="w-60 h-60 bg-grey rounded-3xl">{image}</div>
+  )
+}
+
+export default CuisineImage
