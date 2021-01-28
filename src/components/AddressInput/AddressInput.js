fd802fe66338adb92c71aa96728daa56f975aded
Dhara Patel <dhara.patel95@hotmail.co.uk>
2021-01-28 16:23:58 +0000

    Amend styling on button

--- src/components/AddressInput/AddressInput.js
+++ src/components/AddressInput/AddressInput.js
@@ -4,7 +4,7 @@ const AddressInput = () => {
   return (
     <div className="py-8 flex justify-around items-center w-1/2 mx-auto">
       <span className="text-grey">Find restaurants near you:</span>
-      <div className="rounded bg-white w-1/2 flex py-2 px-1 justify-between">
+      <div className="rounded bg-white w-1/2 flex py-1 pr-1 pl-5 justify-between">
         <input
           className="w-3/5 focus:outline-none"
           name="address"
@@ -12,7 +12,7 @@ const AddressInput = () => {
           placeholder="W13 3RH"
         />
         <button
-          className="submit-button rounded py-1.5 px-4 bg-teal"
+          className="submit-button rounded py-2 px-8 bg-teal hover:bg-teal-hover active:bg-teal-click focus:outline-none"
           type="button"
           value="Find"
         >
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ['active']
+    },
   },
   plugins: [],
 
@@ -15,7 +17,9 @@ module.exports = {
       grey: "#ccc",
       black: "#111",
       white: "#FFF",
-      teal: "#479c9d"
+      teal: "#479c9d",
+      'teal-hover': "#A7D7D7",
+      'teal-click': "#285758"
     },
     container: {
       center: true,
