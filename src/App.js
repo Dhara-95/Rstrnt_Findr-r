9487e1e2e5787ef8f0724b4bb6158e1e95b6fa43
Dhara Patel <dhara.patel95@hotmail.co.uk>
2021-01-28 12:06:09 +0000

    Wrapped homepage elements in a container and center aligned it

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App bg-black">
+    <div className="App container w-4/5">
       <h1 className="text-5xl text-grey py-6" >Rstrnt Findr-r</h1>
-      <div className="py-8">
+      <div className="py-8 flex justify-center">
         <span className="text-grey">Find restaurants near you:</span>
         <form>
           <input name="address" type="text" />
@@ -14,7 +14,7 @@ function App() {
       </div >
       <div className="py-8">
         <h2 className="text-2xl text-grey">Find By Cuisine</h2>
-        <div className="image-container text-grey">
+        <div className="image-container text-grey flex justify-around">
           <div>image 1</div>
           <div>image 2</div>
           <div>image 3</div>
