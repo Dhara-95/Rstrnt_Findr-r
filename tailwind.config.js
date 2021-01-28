b55ca27ab26e304a75701a799df82050d6ebf53d
Dhara Patel <dhara.patel95@hotmail.co.uk>
2021-01-28 11:49:01 +0000

    Added custom colours to font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
   variants: {
     extend: {},
   },
   plugins: [],
+
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      grey: '#ccc',
+      black: '#111',
+    }
+  }
 };
