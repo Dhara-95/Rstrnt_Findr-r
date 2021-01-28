4c03078bec94e7bf9000ac19464d4332549fb9fd
Dhara Patel <dhara.patel95@hotmail.co.uk>
2021-01-28 11:49:01 +0000

    Added custom colours to font

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Rstrnt Findr-r</h1>
-      <span>Find restaurants near you:</span>
+    <div className="App bg-black">
+      <h1 className="text-5xl text-grey" >Rstrnt Findr-r</h1>
+      <span className="text-grey">Find restaurants near you:</span>
       <form>
         <input name="address" type="text" />
         <input className="submit-button" value="Find" type="submit" />
       </form>
-      <h2>Find By Cuisine</h2>
-      <div className="image-container">
+      <h2 className="text-2xl text-grey">Find By Cuisine</h2>
+      <div className="image-container text-grey">
         <div>image 1</div>
         <div>image 2</div>
         <div>image 3</div>
