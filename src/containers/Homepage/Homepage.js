39181380aa72957e45a0ad0d58b07e487d836372
Dhara Patel <dhara.patel95@hotmail.co.uk>
2021-01-28 15:59:54 +0000

    Split divs into components

--- src/containers/Homepage/Homepage.js
+++ src/containers/Homepage/Homepage.js
@@ -1,26 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import AddressInput from "../../components/AddressInput/AddressInput";
+import CuisineList from "../CuisineList/CuisineList";
 
 const Homepage = () => {
-  const [imageArray, updateImageArray] = useState([
-    "image 1",
-    "image 2",
-    "image 3",
-    "image 4",
-  ]);
 
   return (
     <>
       <AddressInput />
-
-      <div className="py-8">
-        <h2 className="text-2xl text-grey p-6">Find By Cuisine</h2>
-        <div className="image-container text-grey flex justify-around">
-          {imageArray.map((image) => {
-            return <div className="w-60 h-60 bg-grey rounded-3xl">{image}</div>;
-          })}
-        </div>
-      </div>
+      <CuisineList />
     </>
   );
 };
