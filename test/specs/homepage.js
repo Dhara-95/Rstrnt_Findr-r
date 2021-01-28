374f28f891769e81e78f9d734462e49194147d87
Chris Cooney <chris.cooney05@hotmail.co.uk>
2021-01-28 09:46:36 +0000

    Added new test to check h1 rendered + updated config.yml to hopefully start server

--- test/specs/homepage.js
+++ test/specs/homepage.js
@@ -1,8 +1,12 @@
 describe("homepage", () => {
   it("should have the right title", () => {
     browser.url("http://localhost:3000");
-    expect(browser).toHaveTitle(
-      "Rstrnt Findr-r"
-    );
+    expect(browser).toHaveTitle("Rstrnt Findr-r");
+  });
+
+  it("Should have the correct page header", () => {
+    browser.url("http://localhost:3000");
+    const header = $("h1=Rstrnt Findr-r");
+    expect(header).toBeDisplayed();
   });
 });
