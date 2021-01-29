efe1326a29fce49d30561f3e8fa9eebba39a9526
Dhara Patel <dhara.patel95@hotmail.co.uk>
2021-01-29 15:22:45 +0000

    Amended responsivness to cuisine image container

--- src/components/CuisineImage/index.js
+++ src/components/CuisineImage/index.js
@@ -4,7 +4,7 @@ const CuisineImage = (props) => {
   const { image } = props;
 
   return (
-    <div className="w-60 h-60 bg-grey rounded-3xl text-black">{image}</div>
+    <div className="w-60 h-60 bg-grey rounded-3xl text-black m-2">{image}</div>
   );
 };
 
--- src/containers/CuisineList/index.js
+++ src/containers/CuisineList/index.js
@@ -6,7 +6,7 @@ const CuisineList = () => {
   const imageArray = ["image 1", "image 2", "image 3", "image 4"];
 
   return (
-    <div className="image-container text-grey flex justify-around">
+    <div className="image-container text-grey flex flex-col items-center justify-center lg:flex-row xl:flex-row 2xl:flex-row">
       {imageArray.map((image, i) => {
         return <CuisineImage image={image} key={i} />;
       })}
