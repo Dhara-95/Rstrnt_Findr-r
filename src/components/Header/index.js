fb3dbb21f94384a6e605b8f0e07679c63ae48bc9
Chris Cooney <chris.cooney05@hotmail.co.uk>
2021-01-29 13:51:21 +0000

    Removed unwanted css + fixed spacing for package imports

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,11 +3,7 @@ import React from "react";
 import { headerLabel } from "../../Labels";
 
 const Header = () => {
-  return (
-    <>
-      <h1 className="text-5xl text-grey py-6">{headerLabel}</h1>
-    </>
-  );
+  return <h1 className="text-5xl text-grey py-6">{headerLabel}</h1>;
 };
 
 export default Header;
