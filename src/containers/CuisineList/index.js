16c21daefb33e39e51afe461b820e4bc6d063fbf
Chris Cooney <chris.cooney05@hotmail.co.uk>
2021-01-29 13:51:21 +0000

    Removed unwanted css + fixed spacing for package imports

--- src/containers/CuisineList/index.js
+++ src/containers/CuisineList/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+
 import CuisineImage from "../../components/CuisineImage";
 import { findByCuisineLabel } from "../../Labels";
 
--- src/containers/Homepage/index.js
+++ src/containers/Homepage/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import LocationSearchForm from "../LocationSearchForm";
 import CuisineList from "../CuisineList";
 
