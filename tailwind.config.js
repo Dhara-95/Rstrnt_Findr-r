351d0a4ede5992b8270f704cf8548b78e1094cd9
Chris Cooney <chris.cooney05@hotmail.co.uk>
2021-01-29 15:00:51 +0000

    Changed locationSearchForm to work with mobiles and tablets

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   variants: {
     extend: {
-      backgroundColor: ['active']
+      backgroundColor: ["active"],
     },
   },
   plugins: [],
@@ -18,11 +18,18 @@ module.exports = {
       black: "#111",
       white: "#FFF",
       teal: "#479c9d",
-      'teal-hover': "#A7D7D7",
-      'teal-click': "#285758"
+      "teal-hover": "#A7D7D7",
+      "teal-click": "#285758",
     },
     container: {
       center: true,
+    },
+    screens: {
+      sm: { min: "640px", max: "767px" },
+      md: { min: "768px", max: "1023px" },
+      lg: { min: "1024px", max: "1279px" },
+      xl: { min: "1280px", max: "1535px" },
+      "2xl": { min: "1536px" },
     },
   },
 };
