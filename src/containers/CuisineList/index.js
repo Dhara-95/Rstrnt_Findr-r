84bd7418481dd1a8f10046856fa677dfd481541a
Dhara Patel <dhara.patel95@hotmail.co.uk>
2021-01-29 14:20:05 +0000

    Split cuisine list map function into own container

--- src/containers/CuisineList/index.js
+++ src/containers/CuisineList/index.js
@@ -1,24 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 
 import CuisineImage from "../../components/CuisineImage";
-import { findByCuisineLabel } from "../../Labels";
 
 const CuisineList = () => {
-  const [imageArray, updateImageArray] = useState([
-    "image 1",
-    "image 2",
-    "image 3",
-    "image 4",
-  ]);
+  const imageArray = ["image 1", "image 2", "image 3", "image 4"];
 
   return (
-    <div className="py-8">
-      <h2 className="text-2xl text-grey p-6">{findByCuisineLabel}</h2>
-      <div className="image-container text-grey flex justify-around">
-        {imageArray.map((image) => {
-          return <CuisineImage image={image} />;
-        })}
-      </div>
+    <div className="image-container text-grey flex justify-around">
+      {imageArray.map((image, i) => {
+        return <CuisineImage image={image} key={i} />;
+      })}
     </div>
   );
 };
