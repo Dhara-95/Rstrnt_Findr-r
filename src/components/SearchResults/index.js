a295fad089c584c3ea649d850abf72c9d5d0062b
Dhara Patel <dhara.patel95@hotmail.co.uk>
2021-02-01 11:42:05 +0000

    Added routing with React Router

---
+++ src/components/SearchResults/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { searchResultQueryLabel } from "../../Labels";
+
+const SearchResults = () => {
+  return (
+    <div>
+      <h2> {searchResultQueryLabel} </h2>
+    </div>
+  );
+};
+
+export default SearchResults;
