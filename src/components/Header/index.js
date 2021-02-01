df970de2654351cadab6f82c42770e63d4353493
Chris Cooney <chris.cooney05@hotmail.co.uk>
2021-02-01 14:04:57 +0000

    Able to go to correct route with user input stored ready for API call

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,24 +1,8 @@
 import React from "react";
-import { useQuery, gql } from "@apollo/client";
 
 import { headerLabel } from "../../Labels";
 
-const YELP_QUERY = gql`
-  {
-    search(term: "burrito", location: "san francisco", limit: 5) {
-      total
-      business {
-        name
-        url
-      }
-    }
-  }
-`;
-
 const Header = () => {
-  const { loading, error, data } = useQuery(YELP_QUERY);
-  if (data) console.log(data);
-
   return <h1 className="text-5xl text-grey py-6">{headerLabel}</h1>;
 };
 
