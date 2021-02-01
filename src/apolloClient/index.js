b1c2b147150e36e5d05cc2089ad878f578dc3491
Dhara Patel <dhara.patel95@hotmail.co.uk>
2021-02-01 10:59:33 +0000

    Managed to access data from yelp api

--- src/apolloClient/index.js
+++ src/apolloClient/index.js
@@ -2,7 +2,7 @@ import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: "https://api.yelp.com/v3/graphql",
+  uri: "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
@@ -11,7 +11,7 @@ const authLink = setContext((_, { headers }) => {
       ...headers,
       authorization:
         "Bearer S1Zn98krZ67DXy-C4ZByWC-hwfPsUu8El_amtr7CX5xjZJ07d-qrhawcvyOBzx1WwKzqK6bfEN7dNu1ynU2b8AcCN5nQGFtQvl0oHpH-g9Wlc2oyGi9jQMzeVtwOYHYx",
-      credentials: "include",
+      "Accept-Language": "en-US",
     },
   };
 });
