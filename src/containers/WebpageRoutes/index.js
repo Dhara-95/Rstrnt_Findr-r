06bf2e9775eb786aefcece47ba4324cbb33f74bf
Chris Cooney <chris.cooney05@hotmail.co.uk>
2021-02-01 15:58:18 +0000

    Moved the react-router Switch into its own component

---
+++ src/containers/WebpageRoutes/index.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { Switch, Route } from "react-router-dom";
+
+import { SearchResults } from "../../components/SearchResults";
+import { Homepage } from "../Homepage";
+
+const WebPageRoutes = () => {
+  const [userInput, setUserInput] = useState("");
+
+  return (
+    <Switch>
+      <Route
+        path={"/"}
+        exact
+        render={() => <Homepage setUserInput={setUserInput} />}
+      />
+      <Route
+        path={"/searchresults"}
+        exact
+        render={() => <SearchResults userInput={userInput} />}
+      />
+    </Switch>
+  );
+};
+
+export { WebPageRoutes };
