f3dd61c09084d36bd7ace54ac03218683440bb54
Chris Cooney <chris.cooney05@hotmail.co.uk>
2021-02-02 16:05:27 +0000

    Finally mocking the Api call!!

--- test/specs/features.js
+++ test/specs/features.js
@@ -1,20 +1,38 @@
-// describe("Feature tests", () => {
-//   const homepageUrl = "http://localhost:3000";
+describe("Feature tests", () => {
+  const homepageUrl = "http://localhost:3000";
 
-//   it("Has the correct url on postcode submit", () => {
-//     browser.url(homepageUrl);
-//     const submitButton = $(".submit-button");
-//     submitButton.click();
-//     expect(browser).toHaveUrlContaining("/searchresults");
-//   });
+  beforeEach(() => {
+    const mockApiCall = browser.mock(
+      "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/graphql",
+      {
+        method: "post",
+        headers: {
+          authorization:
+            "Bearer S1Zn98krZ67DXy-C4ZByWC-hwfPsUu8El_amtr7CX5xjZJ07d-qrhawcvyOBzx1WwKzqK6bfEN7dNu1ynU2b8AcCN5nQGFtQvl0oHpH-g9Wlc2oyGi9jQMzeVtwOYHYx",
+          "Accept-Language": "en-US",
+        },
+      }
+    );
+    mockApiCall.respond({
+      statusCode: 200,
+      fetchResponse: false,
+    });
+  });
 
-//   it("Has correct user input displayed on submit", () => {
-//     browser.url(homepageUrl);
-//     const input = $('[name="postcode"]');
-//     input.addValue("W13 3RH");
-//     const submitButton = $(".submit-button");
-//     submitButton.click();
-//     const h2header = $("h2=Restaurants near W13 3RH");
-//     expect(h2header).toBeDisplayed();
-//   });
-// });
+  it("Has the correct url on postcode submit", () => {
+    browser.url(homepageUrl);
+    const submitButton = $(".submit-button");
+    submitButton.click();
+    expect(browser).toHaveUrlContaining("/searchresults");
+  });
+
+  it("Has correct user input displayed on submit", () => {
+    browser.url(homepageUrl);
+    const input = $('[name="postcode"]');
+    input.addValue("W13 3RH");
+    const submitButton = $(".submit-button");
+    submitButton.click();
+    const h2header = $("h2=Restaurants near W13 3RH");
+    expect(h2header).toBeDisplayed();
+  });
+});
