ad0d3fec163de3ff5de2fa2ff9b52b8bf84d4cda
Dhara Patel <dhara.patel95@hotmail.co.uk>
2021-02-02 10:40:24 +0000

    Feature test for correct user input displayed passing

--- test/specs/features.js
+++ test/specs/features.js
@@ -21,10 +21,18 @@ describe("Feature tests", () => {
 
   it("Has the correct url on postcode submit", () => {
     browser.url(homepageUrl);
-    // const input = $('[name="postcode"]');
-    // input.addValue("W13 3RH");
     const submitButton = $(".submit-button");
     submitButton.click();
     expect(browser).toHaveUrlContaining("/searchresults");
+  });
+
+  it("Has correct user input displayed on submit", () => {
+    browser.url(homepageUrl);
+    const input = $('[name="postcode"]');
+    input.addValue("W13 3RH");
+    const submitButton = $(".submit-button");
+    submitButton.click();
+    const h2header = $("h2=Restaurants near W13 3RH");
+    expect(h2header).toBeDisplayed();
   });
 });
