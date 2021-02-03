514ac2d5b6d4ef31ecb3aa3be368793ecc7f51d1
Chris Cooney <chris.cooney05@hotmail.co.uk>
2021-02-03 10:08:28 +0000

    removed the fetch function unto its own folder + wraped in try catch

---
+++ src/components/SearchResults/searchResultsUtils.js
@@ -0,0 +1,29 @@
+import { RESTAURANT_BY_POSTCODE } from "../../queries";
+
+const fetchRestaurantData = async (userInput) => {
+  try {
+    const rawResponse = await fetch(
+      "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/graphql",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: process.env.REACT_APP_YELP_API_KEY,
+          "Accept-Language": "en-US",
+        },
+        body: JSON.stringify({
+          query: RESTAURANT_BY_POSTCODE,
+          variables: {
+            postcode: userInput,
+          },
+        }),
+      }
+    );
+    const content = await rawResponse.json();
+    console.log(content);
+  } catch (error) {
+    console.log("There was an error with the fetch: " + error);
+  }
+};
+
+export { fetchRestaurantData };
