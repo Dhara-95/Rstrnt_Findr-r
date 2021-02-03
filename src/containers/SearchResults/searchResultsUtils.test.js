623c8d8c984fc92753dc95058fdfc7b6da3cbf4b
Chris Cooney <chris.cooney05@hotmail.co.uk>
2021-02-03 15:07:52 +0000

    Added tests for the checkValieImageURL helper function

---
+++ src/containers/SearchResults/searchResultsUtils.test.js
@@ -0,0 +1,15 @@
+import { checkValidImageURL } from "./searchResultsUtils";
+import { LabelsObject } from "../../Labels";
+
+describe("checkValidImageURL tests", () => {
+  test("Returns passed in valid url", () => {
+    const workingURL = "valid URL";
+    expect(checkValidImageURL(workingURL)).toEqual(workingURL);
+  });
+
+  test("Returns url for place holder img on invalid url", () => {
+    const { invalidImageURLLabel } = LabelsObject;
+    const invalidURL = "https://s3-media3.fl.yelpcdn.com/bphoto/None/o.jpg";
+    expect(checkValidImageURL(invalidURL)).toEqual(invalidImageURLLabel);
+  });
+});
