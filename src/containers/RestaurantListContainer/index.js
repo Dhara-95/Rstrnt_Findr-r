39d9fc585716dbab007c4dc7cf013c7960ee291d
Dhara Patel <dhara.patel95@hotmail.co.uk>
2021-02-03 16:02:34 +0000

    Added comma separated cuisine tags

--- src/containers/RestaurantListContainer/index.js
+++ src/containers/RestaurantListContainer/index.js
@@ -10,6 +10,13 @@ const RestaurantListContainer = (props) => {
         return (
           <div key={i}>
             <h2 className="text-grey">{restaurant.name}</h2>
+            {restaurant.categories.map((tag, i) => {
+              return (
+                <span className="text-grey">
+                  {(i ? ", " : "") + tag.title}{" "}
+                </span>
+              );
+            })}
 
             <img
               src={checkValidImageURL(restaurant.photos[0])}
