23806598d6f4bd61bc4bccf26def908778ea3f0e
Chris Cooney <chris.cooney05@hotmail.co.uk>
2021-02-03 10:08:28 +0000

    removed the fetch function unto its own folder + wraped in try catch

--- src/components/SearchResults/index.js
+++ src/components/SearchResults/index.js
@@ -1,36 +1,14 @@
 import React, { useEffect } from "react";
 
+import { fetchRestaurantData } from "./searchResultsUtils";
 import { LabelsObject } from "../../Labels";
-import { RESTAURANT_BY_POSTCODE } from "../../queries";
 
 const SearchResults = (props) => {
   const { userInput } = props;
   const { searchResultQueryLabel } = LabelsObject;
 
   useEffect(() => {
-    const fetchData = async () => {
-      const rawResponse = await fetch(
-        "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/graphql",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: process.env.REACT_APP_YELP_API_KEY,
-            "Accept-Language": "en-US",
-          },
-          body: JSON.stringify({
-            query: RESTAURANT_BY_POSTCODE,
-            variables: {
-              postcode: userInput,
-            },
-          }),
-        }
-      );
-      const content = await rawResponse.json();
-
-      console.log(content);
-    };
-    fetchData();
+    fetchRestaurantData(userInput);
   }, [userInput]);
 
   return (
