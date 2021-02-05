39d94b33e50d15e2f7f60db333f700396a9c8764
Dhara Patel <dhara.patel95@hotmail.co.uk>
2021-02-05 10:16:04 +0000

    Rename formatAddress test and add test for no escape sequence

--- src/__tests__/formatAddress.test.js
+++ src/__tests__/formatAddress.test.js
@@ -1,9 +1,16 @@
 import { formatAddress } from "../utils/formatAddress";
 
-describe("checkFormattedAddress", () => {
-  test("Returns the correct formatted address", () => {
+describe("formatAddress", () => {
+  test("replaces an escape sequence with English grammar", () => {
     const address = "55 Manor Road\nLondon W13 0LJ\nUnited Kingdom";
-    const correctAddress = "55 Manor Road, London W13 0LJ, United Kingdom";
-    expect(formatAddress(address)).toEqual(correctAddress);
+    const call = formatAddress(address);
+    const expectation = "55 Manor Road, London W13 0LJ, United Kingdom";
+    expect(call).toEqual(expectation);
+  });
+
+  test("no changes made if there is no escape sequence", () => {
+    const address = "55 Manor Road, London W13 0LJ, United Kingdom";
+    const call = formatAddress(address);
+    expect(call).toEqual(address);
   });
 });
