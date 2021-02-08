6cc5781a01c06abdec1655c376a1feab2eab3d5b
Chris Cooney <chris.cooney05@hotmail.co.uk>
2021-02-08 15:06:14 +0000

    Refactor FetchData class for clarity, bug in useEffect not waiting for data to fetch

--- src/utils/fetchRestaurantData.js
+++ src/utils/fetchRestaurantData.js
@@ -9,32 +9,53 @@ class FetchData {
       Authorization: process.env.REACT_APP_YELP_API_KEY,
       "Accept-Language": "en-US",
     };
-    this.query = RESTAURANT_BY_POSTCODE;
   }
 
-  async getData(input, updateRestaurantData, updateLoadingState) {
+  getData(input, updateLoadingState) {
     updateLoadingState(true);
-    try {
-      const rawResponse = await fetch(this.url, {
-        method: this.method,
-        headers: this.headers,
-        body: JSON.stringify({
-          query: this.query,
-          variables: {
-            postcode: input,
-          },
-        }),
-      });
-      const content = await rawResponse.json();
-      const { business } = content.data.search;
-      updateRestaurantData(business);
-    } catch (error) {
-      console.log("There was an error with the fetch: " + error);
-    }
-    updateLoadingState(false);
+    const options = this._formatOptions(input);
+    let returnData;
+
+    fetch(this.url, options)
+      .then((response) => response.json())
+      .then((data) => {
+        updateLoadingState(false);
+        console.log(data.data.search.business);
+        return data.data.search.business;
+      })
+      .catch((error) =>
+        console.log("There was an error with the fetch: " + error)
+      );
+
+    // const rawResponse = await fetch(this.url, {
+    //   method: this.method,
+    //   headers: this.headers,
+    //   body: JSON.stringify({
+    //     query: this.query,
+    //     variables: {
+    //       postcode: input,
+    //     },
+    //   }),
+    // });
+    // const content = await rawResponse.json();
+    // const { business } = content.data.search;
+  }
+
+  // tried to use # to make this method private but having Babel issues
+  _formatOptions(input) {
+    return {
+      method: this.method,
+      headers: this.headers,
+      body: JSON.stringify({
+        query: RESTAURANT_BY_POSTCODE,
+        variables: {
+          postcode: input,
+        },
+      }),
+    };
   }
 }
 
-const restaurantFetch = new FetchData();
+const yelp = new FetchData();
 
-export { restaurantFetch };
+export { yelp };
