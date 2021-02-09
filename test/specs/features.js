7f90ccf2ab41cfabe9bb308d7b07fa11e2af0f9d
Chris Cooney <chris.cooney05@hotmail.co.uk>
2021-02-09 14:53:40 +0000

    Add feature test to check filter by rating is working

--- test/specs/features.js
+++ test/specs/features.js
@@ -30,4 +30,17 @@ describe("Feature tests", () => {
     const h2header = $("h2=Restaurants near W13 3RH");
     expect(h2header).toBeDisplayed();
   });
+
+  it("Has the ability to change the restaurant order based on rating", () => {
+    browser.url(homepageUrl);
+    const input = $('[name="postcode"]');
+    input.addValue("W13 3RH");
+    const submitButton = $(".submit-button");
+    submitButton.click();
+    const selectBox = $("#filter");
+    const elem = $("h2='A 2 Zee's'");
+    expect(elem).toBeDisplayedInViewport();
+    selectBox.selectByAttribute("value", "rating");
+    expect(elem).not.toBeDisplayedInViewport();
+  });
 });
