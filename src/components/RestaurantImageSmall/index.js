1f07cbb4b7527e7ae3faf3c0b1afe81e4d155652
Dhara Patel <dhara.patel95@hotmail.co.uk>
2021-02-09 10:03:35 +0000

    Remove mask styling from css to tailwind css

--- src/components/RestaurantImageSmall/index.js
+++ src/components/RestaurantImageSmall/index.js
@@ -4,11 +4,11 @@ import { checkInvalidImageURL } from "../../utils/checkInvalidImageURL";
 
 const RestaurantImageSmall = (props) => {
   const { url, name } = props;
-  const validURL = checkInvalidImageURL(url);
+  const src = checkInvalidImageURL(url);
 
   return (
-    <div className="mask">
-      <img className="image" src={validURL} alt={name} />
+    <div className="flex w-48 h-48 rounded-3xl overflow-hidden relative justify-center content-center ">
+      <img className="image" src={src} alt={name} />
     </div>
   );
 };
