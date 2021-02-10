9ad650da5efecd6b283a188392fc90693547b804
Chris Cooney <chris.cooney05@hotmail.co.uk>
2021-02-10 13:55:51 +0000

    Refactor homepage.js test to one single feature test

--- test/specs/homepage.js
+++ test/specs/homepage.js
@@ -1,44 +1,22 @@
 describe("homepage", () => {
   const homepageUrl = "http://localhost:3000";
 
-  it("has the right title", () => {
+  it("Has all the expected elements rendered for the user", () => {
     browser.url(homepageUrl);
-    expect(browser).toHaveTitle("Rstrnt Findr-r");
-  });
 
-  it("has the correct page header", () => {
-    browser.url(homepageUrl);
     const header = $("h1=Rstrnt Findr-r");
-    expect(header).toBeDisplayed();
-  });
-
-  it("has an input field for user postcode", () => {
-    browser.url(homepageUrl);
+    const inputLabel = $("span=Find restaurants near you:");
     const inputField = $('[name="postcode"]');
-    expect(inputField).toExist();
-  });
-
-  it("has correct text next to input field", () => {
-    browser.url(homepageUrl);
-    const span = $("span=Find restaurants near you:");
-    expect(span).toBeDisplayed();
-  });
-
-  it("has a find button", () => {
-    browser.url(homepageUrl);
-    const button = $('[value="Find"]');
-    expect(button).toExist();
-  });
-
-  it("has a header of find by cuisine", () => {
-    browser.url(homepageUrl);
+    const submitButton = $('[value="Find"]');
     const findByCuisineTitle = $("h2=Find By Cuisine");
-    expect(findByCuisineTitle).toBeDisplayed();
-  });
-
-  it("has a container with 4 elements in it", () => {
-    browser.url(homepageUrl);
     const imageContainer = $(".image-container");
+
+    expect(browser).toHaveTitle("Rstrnt Findr-r");
+    expect(header).toBeDisplayed();
+    expect(inputLabel).toBeDisplayed();
+    expect(inputField).toBeDisplayed();
+    expect(submitButton).toBeDisplayed();
+    expect(findByCuisineTitle).toBeDisplayed();
     expect(imageContainer).toHaveChildren(4);
   });
 });
