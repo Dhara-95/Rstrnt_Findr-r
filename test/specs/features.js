77fb31fc547bed8bd450b24aa318c83cdeed75f4
Chris Cooney <chris.cooney05@hotmail.co.uk>
2021-02-10 13:39:44 +0000

    refactor tests to keep code dry

--- test/specs/features.js
+++ test/specs/features.js
@@ -2,6 +2,12 @@ import { business } from "../../__mocks__/dataMock";
 
 describe("Feature tests", () => {
   const homepageUrl = "http://localhost:3000";
+  const elementXPathBase = "//*[@id='root']/div/div/div";
+  const restaurantName = "h2.restaurant-name";
+  const restaurantTag = "div.restaurant-tag";
+  const restaurantPrice = "h2.restaurant-price";
+  const restaurantRating = "div.restaurant-rating";
+  const restaurantAddress = "span.restaurant-address";
 
   beforeEach(() => {
     const mockApiCall = browser.mock(
@@ -40,22 +46,20 @@ describe("Feature tests", () => {
   });
 
   it("Has the correct mocked data displayed", () => {
-    const elementXPath = "//*[@id='root']/div/div/div[4]";
-
     browser.url(homepageUrl);
     const input = $('[name="postcode"]');
     input.addValue("W13 3RH");
     const submitButton = $(".submit-button");
     submitButton.click();
 
-    $(elementXPath).waitForExist();
-    const restaurantCard = $(elementXPath);
+    $(`${elementXPathBase}[4]`).waitForExist();
+    const restaurantCard = $(`${elementXPathBase}[4]`);
 
-    const name = restaurantCard.$("h2.restaurant-name");
-    const tag = restaurantCard.$("div.restaurant-tag");
-    const price = restaurantCard.$("h2.restaurant-price");
-    const rating = restaurantCard.$("div.restaurant-rating");
-    const address = restaurantCard.$("span.restaurant-address");
+    const name = restaurantCard.$(restaurantName);
+    const tag = restaurantCard.$(restaurantTag);
+    const price = restaurantCard.$(restaurantPrice);
+    const rating = restaurantCard.$(restaurantRating);
+    const address = restaurantCard.$(restaurantAddress);
 
     expect(name).toHaveText("Chic O Land");
     expect(tag).toHaveText("Fast Food");
@@ -67,8 +71,6 @@ describe("Feature tests", () => {
   });
 
   it("Has mocked data ordered as expected", () => {
-    const elementXPathBase = "//*[@id='root']/div/div/div";
-
     browser.url(homepageUrl);
     const input = $('[name="postcode"]');
     input.addValue("W13 3RH");
@@ -80,9 +82,9 @@ describe("Feature tests", () => {
     const restaurantCard2 = $(`${elementXPathBase}[3]`);
     const restaurantCard3 = $(`${elementXPathBase}[4]`);
 
-    const name1 = restaurantCard1.$("h2.restaurant-name");
-    const name2 = restaurantCard2.$("h2.restaurant-name");
-    const name3 = restaurantCard3.$("h2.restaurant-name");
+    const name1 = restaurantCard1.$(restaurantName);
+    const name2 = restaurantCard2.$(restaurantName);
+    const name3 = restaurantCard3.$(restaurantName);
 
     expect(name1).toHaveText("A 2 Zee's");
     expect(name2).toHaveText("Tophill Caribean");
