4e763cbfd32f2db2096b9a2b65bb61adc3444d40
Chris Cooney <chris.cooney05@hotmail.co.uk>
2021-02-24 13:36:53 +0000

    Update snapshot tests to reflect props change

--- src/containers/__tests__/searchResults.test.js
+++ src/containers/__tests__/searchResults.test.js
@@ -1,11 +1,19 @@
 import React from "react";
 import renderer from "react-test-renderer";
 
+import { businesses } from "../../mocks/dataMock";
 import { SearchResults } from "../SearchResults";
 
 it("displays the user input correctly", async () => {
   const tree = renderer
-    .create(<SearchResults userInput={"W13 3RH"} />)
+    .create(
+      <SearchResults
+        userInput={"W13 3RH"}
+        isLoading={false}
+        data={businesses}
+        handleOnChange={"function"}
+      />
+    )
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
