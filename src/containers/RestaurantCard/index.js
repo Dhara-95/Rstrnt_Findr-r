341617821cb59c920b2f21ad09a4923822843b80
Dhara Patel <dhara.patel95@hotmail.co.uk>
2021-02-24 10:20:15 +0000

    Removed <div> around RestaurantImageSmall in RestaurantCard

--- src/containers/RestaurantCard/index.js
+++ src/containers/RestaurantCard/index.js
@@ -25,11 +25,9 @@ const RestaurantCard = (props) => {
 
   return (
     <div className="restaurant-card flex mx-auto my-4 w-full xl:w-4/5 2xl:w-4/5">
-      <div className="flex justify-center w-1/5">
-        <RestaurantImageSmall url={photos[0]} name={name} />
-      </div>
+      <RestaurantImageSmall url={photos[0]} name={name} />
 
-      <div className="flex flex-col w-full text-left self-center md:w-1/3 lg:w-1/3 xl:w-1/2 2xl:w-1/2 space-y-4">
+      <div className="flex flex-col w-full text-left self-center px-4 md:w-1/3 lg:w-1/3 xl:w-1/2 2xl:w-1/2 space-y-4">
         <div className="pb-4">
           <RestaurantName name={name} />
 
