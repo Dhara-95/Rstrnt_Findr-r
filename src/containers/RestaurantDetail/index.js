5aad26278a78025b84c5c51eac71c8ed29a50938
Dhara Patel <dhara.patel95@hotmail.co.uk>
2021-02-26 11:22:40 +0000

    Add restaurant image to /restaurant/name route

--- src/containers/RestaurantDetail/index.js
+++ src/containers/RestaurantDetail/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 
-import { RestaurantLocationMap } from "../../components";
+import { RestaurantLocationMap, RestaurantImageLarge } from "../../components";
 
 const RestaurantDetail = (props) => {
-  const { coordinates, name } = props.restaurant;
+  const { coordinates, name, photos } = props.restaurant;
 
   return (
     <>
       <h2 className="text-grey">Restaurant Detail page</h2>
+      <RestaurantImageLarge url={photos[0]} name={name} />
       <RestaurantLocationMap coordinates={coordinates} name={name} />
     </>
   );
