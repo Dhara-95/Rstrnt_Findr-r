2f240e3df7faa69082ea05337ca3c8609bce8ac1
Dhara Patel <dhara.patel95@hotmail.co.uk>
2021-02-26 11:35:39 +0000

    Amend width, height, padding and responsiveness of image

--- src/components/RestaurantImageLarge/index.js
+++ src/components/RestaurantImageLarge/index.js
@@ -7,12 +7,8 @@ const RestaurantImageLarge = (props) => {
   const src = checkInvalidImageURL(url);
 
   return (
-    <div className="flex w-48 h-48 rounded-3xl overflow-hidden relative justify-center content-center ">
-      <img
-        className="w-auto h-full block object-cover transform scale-130"
-        src={src}
-        alt={name}
-      />
+    <div className="flex mx-auto w-full h-80 py-8 overflow-hidden relative justify-center content-center lg:w-2/3 xl:w-2/3 2xl:w-2/3 ">
+      <img className="w-full h-auto block object-cover" src={src} alt={name} />
     </div>
   );
 };
