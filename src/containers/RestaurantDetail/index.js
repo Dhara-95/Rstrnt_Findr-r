5de49702496adbd5d9384ce94405e35f7024a9af
Dhara Patel <dhara.patel95@hotmail.co.uk>
2021-03-04 10:06:14 +0000

    Add restaurant large image to /restaurant/:name page

--- src/containers/RestaurantDetail/index.js
+++ src/containers/RestaurantDetail/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import { labels } from "../../Labels";
-import { RestaurantLocationMap, RestaurantName } from "../../components";
+import {
+  RestaurantLocationMap,
+  RestaurantImageLarge,
+  RestaurantName,
+} from "../../components";
 
 const RestaurantDetail = (props) => {
-  const { coordinates, name } = props.restaurant;
+  const { coordinates, name, photos } = props.restaurant;
   const { postcode } = props;
   const { backLink } = labels;
 
@@ -15,6 +19,7 @@ const RestaurantDetail = (props) => {
         {`${backLink} ${postcode}`}
       </Link>
       <RestaurantName name={name} />
+      <RestaurantImageLarge url={photos[0]} name={name} />
       <RestaurantLocationMap coordinates={coordinates} name={name} />
     </>
   );
