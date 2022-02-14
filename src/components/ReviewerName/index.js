5656fbc3758b76f7af9ba47b5ffb71599d2767d6
Dhara Patel <63232864+Dhara-95@users.noreply.github.com>
2022-02-14 16:20:42 +0000

    Reviews (#19)
    * Update query to return userName with reviews
    
    * Render info on screen to confirm we are using correct data
    
    * Add reviewQuote component to deal with adding quotes to each review
    
    * Add ReviewText component to format review text
    
    * Add StarRating to review
    
    * Add ReviewText to format review
    
    * Add long dash for reviewer name (em-dash)
    
    * Add responsive widths for reviews
    
    * Add snapshot test for reviews

---
+++ src/components/ReviewerName/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ReviewerName = (props) => {
+  const { name } = props;
+
+  return <p className="text-grey text-xl">{`— ${name}`}</p>;
+};
+
+export { ReviewerName };
